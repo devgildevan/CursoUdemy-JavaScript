6d9a016a3125d73eeac2e3ba72f73823fbf35908
devgildevan <gildevan87654321@gmail.com>
2022-06-30 14:23:47 -0300

    ColocandoEmpraticaVariaveisComLET

---
+++ Estruturas-de-Programação/1_variaveis/script.js
@@ -0,0 +1,28 @@
+let nome = "Gildevan"
+
+console.log(`Ola ${nome} como você está?`)
+
+let idade = 28
+
+console.log(`A idade do ${nome} é ${idade}anos e está prestes a fazer 29` )
+
+
+nome = "Érika"
+idade ++
+console.log(`A namorada dele a ${nome} tem apenas ${idade} anos`)
+
+
+/*Declarações de varias variaveis usando LET*/
+let quadrado =4, triangulo = 5, retangulo = 3;
+
+console.log(quadrado *(triangulo - retangulo))
+
+console.log(quadrado)
+console.log(triangulo)
+console.log(retangulo)
+
+/*Nesta primeria  aula da unidade 4 temos exemplos de templastes literals,
+ e declarações de variaveis com LET, e acrecentamos novos valores as variaveis.
+ E declarações de varias variaveis com let*/ 
+ 
+
