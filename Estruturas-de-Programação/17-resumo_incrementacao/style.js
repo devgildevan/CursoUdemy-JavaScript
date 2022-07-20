697c950a3cb4d26637ee7a59484874c595613826
devgildevan <gildevan87654321@gmail.com>
2022-07-20 20:57:42 -0300

    Finalização  da Unidade 4

--- Estruturas-de-Programação/17-resumo_incrementacao/style.js
+++ Estruturas-de-Programação/17-resumo_incrementacao/style.js
@@ -1,18 +1,18 @@
 let numero = 5
 while(numero < 50 ){
     console.log(numero)
-    numero += 10
+    numero += 10 /* dessa forma quando queremos colocar um valor maior que 1*/
 } 
 
 
 
 /* maniera mais utilizadas é a maneira baixo e mais usada nos dias atuais*/
 
-for(let i = 0; i < 10; i++){
+for(let i = 0; i < 10; i++){ /* incrementando*/
     console.log(`O i é ${i}`)
 
 }
 
-for(let i = 10; i > 0; i--){
+for(let i = 10; i > 0; i--){ /*decrementando */
     console.log(i)
-}
+} 
---
+++ Estruturas-de-Programação/18-SWITCH/style.js
@@ -0,0 +1,18 @@
+let nome = "Gil"
+
+switch(nome){
+    case "Gil":
+        console.log("O nome é Gil")
+        break 
+
+    case "Pedro":
+        console.log("O nome é Pedro")
+        break
+
+        default:
+            console.log("O nome não foi encontrado!")
+            break
+}
+
+/* É praticamente  mesma coisa do (if  - else)
+só muda mais na questão dos (:)*/
